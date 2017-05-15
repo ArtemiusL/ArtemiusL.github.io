c564f690738abc6fad8d83e3527086512710f254
ArtemiusL <lamber95@icloud.com>
2017-05-15 23:54:44 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var mainHeader = document.querySelector(".header-cover");
+var headerBgFixed = document.querySelector(".header-cover-bg");
+
+
+headerBgFixed.style.height = mainHeader.offsetHeight + "px";
